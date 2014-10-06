41c6d0438b946b0833ead2f011ec2450af658240
qantasmz <qanta_shimizu@a23-0-3-244.deploy.static.akamaitechnologies.com>
2014-10-06 18:15:05 -0400

    detecting type

--- php/ad.php
+++ php/ad.php
@@ -1,5 +1,5 @@
 <?php
-$path = $_FILES['myfile']['tmp_name'];
+$path = "/var/www/html/elload/image/out/c046c3bb9673409a4a07c811e90111e9217f9ec0(.)";
 $mime = shell_exec('file -bi '.escapeshellcmd($path));
 $mime = trim($mime);
 $mime = preg_replace("/ [^ ]*/", "", $mime);
