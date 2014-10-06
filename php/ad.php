dc7d6a202cdf9953a9cf6fc472624e1a66e195ad
qantasmz <qanta_shimizu@a23-0-3-244.deploy.static.akamaitechnologies.com>
2014-10-06 18:21:28 -0400

    ing output

--- php/ad.php
+++ php/ad.php
@@ -3,5 +3,7 @@ $path = "/var/www/html/elload/image/out/c046c3bb9673409a4a07c811e90111e9217f9ec0
 $mime = shell_exec('file -bi '.escapeshellcmd($path));
 $mime = trim($mime);
 $mime = preg_replace("/ [^ ]*/", "", $mime);
-print($mime);
+$typ = 'Content-type: '.$mime;
+header($typ);
+readfile($path);
 ?>
